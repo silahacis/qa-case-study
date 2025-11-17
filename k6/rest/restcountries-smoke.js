79115abc2f9a4e55b9da3cfdb9b579ec542a815e
Sıla Hacıalioğlu <silahacis@outlook.com>
2025-11-17 16:18:57 +0300

    feat(k6): add edge cases test for RestCountries API

--- k6/rest/restcountries-smoke.js
+++ k6/rest/restcountries-smoke.js
@@ -47,5 +47,72 @@ export default function () {
     });
 
     sleep(1);
+
+    // ***EDGE CASES***
+
+    // 1) Non-existing country
+    const invalid = http.get(
+      "https://restcountries.com/v3.1/name/thisCountryDoesNotExist123456"
+    );
+    check(invalid, {
+      "invalid country returns 404 or 200": (r) =>
+        r.status === 404 || r.status === 200,
+    });
+
+    if (invalid.status === 200) {
+      const invalidBody = JSON.parse(invalid.body || "[]");
+      check(invalidBody, {
+        "empty array for invalid country": (b) =>
+          Array.isArray(b) && b.length === 0,
+      });
+    }
+
+    sleep(0.5);
+
+    // 2) Partial match (should return multiple countries)
+    const partial = http.get("https://restcountries.com/v3.1/name/tur");
+    check(partial, {
+      "partial search returns 200": (r) => r.status === 200,
+      "partial search returns multiple results": (r) => {
+        try {
+          const data = JSON.parse(r.body);
+          return Array.isArray(data) && data.length >= 2;
+        } catch {
+          return false;
+        }
+      },
+    });
+
+    sleep(0.5);
+
+    // 3) Invalid numeric input
+    const numeric = http.get("https://restcountries.com/v3.1/name/12345");
+    check(numeric, {
+      "numeric query handled gracefully": (r) =>
+        r.status === 404 || r.status === 200,
+    });
+
+    sleep(0.5);
+
+    // 4) Special characters
+    const specialInput = encodeURIComponent("%$#@!");
+    const special = http.get(
+    `https://restcountries.com/v3.1/name/${specialInput}`
+    );
+
+    check(special, {
+    "special chars do not crash API": (r) =>
+        r.status === 404 || r.status === 200,
+    });
+
+    sleep(0.5);
+
+    // 5) Empty string
+    const emptyQuery = http.get("https://restcountries.com/v3.1/name/");
+    check(emptyQuery, {
+      "empty query returns valid status (400|404|200)": (r) =>
+        r.status === 400 || r.status === 404 || r.status === 200,
+    });
+    sleep(0.5);
   });
 }
